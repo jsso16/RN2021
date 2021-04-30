595c21d9bc0f310e008b655e66325e6fff921ce8
jsso16 <wjsthwls24@daum.net>
2021-04-30 13:22:48 +0900

    Edit index.js(0423)

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
 
 import React from 'react';
 import {AppRegistry} from 'react-native';
-import App from './app/App';
+import App from './app/Midterm';
 import {name as appName} from './app.json';
 
 const TodoApp = () => <App />
