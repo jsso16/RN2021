b643099c7f594126a2fa59e4fc05cd76ff5fd340
jsso16 <wjsthwls24@daum.net>
2021-05-11 12:37:42 +0900

    Edit index.js(0507)

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
  * @format
  */
 
+// import React from 'react';
 // import {AppRegistry} from 'react-native';
 // import App from './App';
 // import {name as appName} from './app.json';
@@ -9,7 +10,8 @@
 import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './app/App';
-import {name as appName} from './app.json';
+import {name as MyApp} from './app.json';
+AppRegistry.registerComponent(MyApp, () => App);
 
-const TodoApp = () => <App />
-AppRegistry.registerComponent(appName, () => TodoApp);
+// const TodoApp = () => <App />
+// AppRegistry.registerComponent(appName, () => TodoApp);
