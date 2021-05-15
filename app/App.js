1b80a8bf3edfb8be57ceaa3914e82d7006eb82b4
jsso16 <wjsthwls24@daum.net>
2021-05-15 13:48:01 +0900

    Edit Profile Card files(0514)

--- app/App.js
+++ app/App.js
@@ -1,14 +1,19 @@
 // 예제 4.7 - 프로필 카드 배경 설정하기
 import React, {Component} from 'react'
-import {StyleSheet, View} from 'react-native'
+import {Image, StyleSheet, View} from 'react-native'
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.cardContainer} />
+        <View style={styles.cardContainer}>
+          <View style={styles.cardImageContainer}>
+            <Image style={styles.cardImage} 
+              source={require('./user.png')} />
+          </View>
+        </View>
       </View>
-    );
+    )
   }
 }
 
@@ -21,8 +26,34 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   cardContainer: {
+    // 예제 4.14 - 프로필 카드 레이아웃 수정하기
+    alignItems: 'center',
+    // 예제 4.10 - 프로필 카드 border 속성 적용하기
+    borderColor: 'black',
+    borderWidth: 3,
+    borderStyle: 'solid',
+    borderRadius: 20,
     backgroundColor: profilecardColor,
     width: 300,
     height: 400
-  }
+  },
+  // 예제 4.10 - 프로필 카드 이미지 적용하기
+  cardImageContainer: {  
+    alignItems: 'center',     
+    backgroundColor: 'white',
+    borderWidth: 3,
+    borderColor: 'black',
+    width: 120,
+    height: 120,
+    borderRadius: 60,
+    // 예제 4.14 - 프로필 카드 레이아웃 수정하기
+    marginTop: 30,
+    paddingTop: 15
+  }, 
+  cardImage: {        
+    width: 80,
+    height: 80
+  } 
 });
+
+export default App
