49db3325ffcd3547e8a1a1da68b5183dc8ef65a7
jsso16 <wjsthwls24@daum.net>
2021-05-25 10:40:55 +0900

    Edit Profile Card files(0521)

--- app/App.js
+++ app/App.js
@@ -1,6 +1,6 @@
 // 예제 4.7 - 프로필 카드 배경 설정하기
 import React, {Component} from 'react'
-import {Image, StyleSheet, View} from 'react-native'
+import {Image, Platform, StyleSheet, Text, View} from 'react-native'
 
 class App extends Component {
   render() {
@@ -10,6 +10,23 @@ class App extends Component {
           <View style={styles.cardImageContainer}>
             <Image style={styles.cardImage} 
               source={require('./user.png')} />
+          </View>
+          <View>
+            <Text style={styles.cardName}>    
+              Sojin Jeon
+            </Text>
+          </View>
+          <View style={styles.cardOccupationContainer}>    
+            <Text style={styles.cardOccupation}>    
+              React Native Developer
+            </Text>
+          </View>
+          <View>
+            <Text style={styles.cardDescription}>    
+              Sojin is a really great JavaScript developer. She
+              loves using JS to build React Native applications
+              for iOS and Android.
+            </Text>
           </View>
         </View>
       </View>
@@ -35,7 +52,20 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     backgroundColor: profilecardColor,
     width: 300,
-    height: 400
+    height: 400,
+    // 예제 5.1 - 프로필 카드에 그림자 추가하기
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: {
+          height: 10
+        },
+        shadowOpacity: 1
+      },
+      android: {
+        elevation: 15
+      }
+    })
   },
   // 예제 4.10 - 프로필 카드 이미지 적용하기
   cardImageContainer: {  
@@ -48,12 +78,66 @@ const styles = StyleSheet.create({
     borderRadius: 60,
     // 예제 4.14 - 프로필 카드 레이아웃 수정하기
     marginTop: 30,
-    paddingTop: 15
+    paddingTop: 15,
+    // 예제 5.1 - 프로필 카드에 그림자 추가하기
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: {
+          height: 10
+        },
+        shadowOpacity: 1
+      },
+      android: {
+        borderWidth: 3,
+        borderColor: 'black',
+        elevation: 15
+      }
+    })
   }, 
   cardImage: {        
     width: 80,
     height: 80
-  } 
+  },
+  // 예제 4.15 - 프로필 카드에 텍스트 추가하기
+  // 예제 4.17 - 텍스트 요소에 폰트 스타일 설정하기
+  cardName: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 24,
+    marginTop: 30,
+    // 예제 4.20 - 프로필 카드 완성하기
+    textShadowColor: 'black',
+    textShadowOffset: {
+      height: 2,
+      width: 2
+    },
+    textShadowRadius: 3,
+    ...Platform.select({
+      ios: {
+        fontFamily: 'American Typewriter'
+      },
+      android: {
+        fontFamily: 'monospace'
+      }
+    })
+  },
+  cardOccupationContainer: {
+    borderColor: 'black',
+    borderBottomWidth: 3
+  }, 
+  cardOccupation: {
+    fontWeight: 'bold',
+    marginTop: 10,
+    marginBottom: 10
+  },
+  cardDescription: {
+    fontStyle: 'italic',
+    marginTop: 10,
+    marginRight: 40,
+    marginLeft: 40,
+    marginBottom: 10
+  }
 });
 
 export default App
